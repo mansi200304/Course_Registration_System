7fe481317b7c9c0361583872edb627e73e5d16d8
AdityaBhardwaj04 <abd.aditya10+github@ouutlook.com>
2024-12-06 16:51:11 +0530

    Changes made

--- backend/models/Course.js
+++ backend/models/Course.js
@@ -1,9 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const courseSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: String, required: true },  // E.g., "3 months"
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: String, required: true }, // E.g., "3 months"
+    courseRequired: {
+        type: String,
+        required: true,
+        enum: ["BTech", "MTech", "PhD"], // This limits the values to BTech, MTech, or PhD
+    },
+    courseType: {
+        type: String,
+        required: true,
+        enum: ["Required", "Elective"], // This limits the values to 'Required' or 'Elective'
+    },
 });
 
-module.exports = mongoose.model('Course', courseSchema);
+module.exports = mongoose.model("Course", courseSchema);
